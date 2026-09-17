55592c4136252b85ba040d5a280eb85f541b4e5c
47umair0101 <47umair0101@gmail.com>
2026-09-17 16:53:25 +0500

    image compression

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -12,7 +12,8 @@ export default function page() {
       <Image src={pic} alt='demo image' width={400} />
       <Image src="https://www.nokia.com/sites/default/files/2025-11/dcn_rack_with_lights_still_ret3-1.jpg" 
       alt='this is desc of image' width={400} height={400}
-      />
+      layout='responsive'
+     />
     </div>
   )
 }
