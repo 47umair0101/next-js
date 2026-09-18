835a5b0e090089c40b62d54a0197c79907694e37
47umair0101 <47umair0101@gmail.com>
2026-09-18 22:03:30 +0500

    next.js advanced concepts

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,29 @@
 
 import "./globals.css";
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import {Roboto} from 'next/font/google';
+// Importing font locally in project
+import Sunshine_Topical from 'next/font/local'
+import Script from "next/script";
 
+const roboto_font = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '900'],
+  variable: '--font-roboto'
+})
+
+const shunshine_font = Sunshine_Topical({
+  src: './fonts/Sunshine_Tropical.ttf',
+  variable: '--font-sunshine'
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    // <html lang="en" className={`${roboto_font.variable} ${shunshine_font.variable}`}>
+    <html lang="en" className={[roboto_font.variable, shunshine_font.variable].join(" ")}>
       <body>
         {children}
+        <Script src={"https://cdn.jsdelivr.net/npm/axios@1.20.0/dist/axios.min.js"}/>
         <ProgressBar
           height="4px"
           color="#107594"
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -7,7 +7,10 @@ export default function page() {
   return (
     <div>
       <Menu />
-      <h1 className='global-text'>This is Profile Page</h1>
+      <h1 className='global-text my-text'>This is Profile Page</h1>
+
+      <h1 className='my-local-text'>This is Profile Page</h1>
+
       {/* <img style={{width: 400}} src='images/demo.jpg' /> */}
       <Image src={pic} alt='demo image' width={400} />
       <Image src="https://www.nokia.com/sites/default/files/2025-11/dcn_rack_with_lights_still_ret3-1.jpg" 
